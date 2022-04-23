1722075ab07f796bfc00c7f8ce9ceed2ca2c6341
Kissele <akiselev@okta-solutions.tech>
2022-04-24 00:29:25 +0300

    Register ui

--- app/src/main/java/com/example/kykachat/MainActivity.kt
+++ app/src/main/java/com/example/kykachat/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kykachat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import com.example.kykachat.activities.RegisterActivity
 import com.example.kykachat.databinding.ActivityMainBinding
 import com.example.kykachat.ui.fragments.ChatsFragment
 import com.example.kykachat.ui.objects.AppDrawer
@@ -26,10 +28,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initFunc() {
-        setSupportActionBar(mToolbar)
-        mAppDrawer.create()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.dataContainer, ChatsFragment()).commit()
+        if (false) {
+            setSupportActionBar(mToolbar)
+            mAppDrawer.create()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.dataContainer, ChatsFragment()).commit()
+        } else {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun initFields() {
--- app/src/main/java/com/example/kykachat/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/kykachat/activities/RegisterActivity.kt
@@ -2,16 +2,30 @@ package com.example.kykachat.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.kykachat.R
 import com.example.kykachat.databinding.ActivityRegisterBinding
+import com.example.kykachat.ui.fragments.auth.PhoneNumberFragment
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityRegisterBinding
+    private lateinit var mToolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mToolbar = mBinding.registerToolbar
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.register_title_your_phone)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.registerDataContainer, PhoneNumberFragment())
+            .commit()
+
     }
 }
